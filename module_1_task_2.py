4752cc377ded88a8bde1a4d8a763ed617075deb1
YuriyTsygankov <jura62@gmail.com>
2024-10-30 11:15:26 +0300

    Add files via upload

---
+++ module_1_task_2.py
@@ -0,0 +1,6 @@
+task_count = 12
+hour_count = 1.5
+course_name = "Python"
+task_time = hour_count / task_count
+print("Курс:", course_name, ", всего задач:", task_count, ", затрачено часов:", hour_count,
+      ", среднее время выполнения", task_time, "часа")
