e35c5b3c8deeff3388bbe7fb19855979ea749484
YuriyTsygankov <jura62@gmail.com>
2024-11-13 14:07:28 +0300

    Add files via upload

---
+++ module_1_task_5.py
@@ -0,0 +1,7 @@
+immutable_var = (1, 2, 3, 4, "a", "b")
+print(immutable_var)
+mutable_list = [1, 2, 3, 4, "a", "b"]
+print(mutable_list)
+mutable_list[-1] = 'Modified'
+mutable_list[0] = 11
+print(mutable_list)
